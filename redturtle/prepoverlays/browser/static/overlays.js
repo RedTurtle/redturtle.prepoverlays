fdc09779fb868ad957c018a55e3e68648cf73066
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-10-28 11:56:37 +0100

    Increase capabilities

--- redturtle/prepoverlays/browser/static/overlays.js
+++ redturtle/prepoverlays/browser/static/overlays.js
@@ -4,19 +4,42 @@
  * This is the javascript that looks for overlays
  */
 (function(jQuery) {
+
+  /*
+   * Initialize overlay for the element
+   */
+  function init_overlay(idx, el, subtype) {
+    var options;
+    subtype = subtype || 'ajax';
+    el = jQuery(el);
+    try {
+      options = JSON.parse(el.attr('data-overlay-options'));
+    } catch (e) {
+      options = {};
+    }
+
+    if (options.subtype === undefined) {
+      options.subtype = subtype;
+    }
+    el.prepOverlay(options);
+  }
+
   /*
    * Apply an overlay to all the links with class infoIco like
    */
-  function prepOverlay() {
-    jQuery('[data-overlay-ajax]').prepOverlay({
-      subtype: 'ajax'
+  function configure_overlays() {
+    jQuery('[data-overlay]').each(function(idx, el) {
+      init_overlay(idx, el);
     });
-    jQuery('[data-overlay-iframe]').prepOverlay({
-      subtype: 'iframe'
+    jQuery('[data-overlay-ajax]').each(function(idx, el) {
+      init_overlay(idx, el, 'ajax');
     });
-    jQuery('[data-overlay-image]').prepOverlay({
-      subtype: 'image'
+    jQuery('[data-overlay-iframe]').each(function(idx, el) {
+      init_overlay(idx, el, 'iframe');
+    });
+    jQuery('[data-overlay-image]').each(function(idx, el) {
+      init_overlay(idx, el, 'image');
     });
   }
-  jQuery(document).ready(prepOverlay);
+  jQuery(document).ready(configure_overlays);
 }(jQuery));
