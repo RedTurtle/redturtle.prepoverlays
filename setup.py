479b2f5cdd77577fff8afc6a21a11f426aecdfbb
Alessandro Pisa <alessandro.pisa@redturtle.it>
2015-11-30 16:52:50 +0100

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = "\n\n".join((
 
 setup(
     name='redturtle.prepoverlays',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description="Trigger Plone overlays by using data attributes",
     long_description=long_description,
     # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
